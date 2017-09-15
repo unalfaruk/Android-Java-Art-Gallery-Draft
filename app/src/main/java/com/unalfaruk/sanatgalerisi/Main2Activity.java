ffd4a978e807e9e7bfde86fba2d9b209e0b75de9
Faruk UNAL <ahmet@unalfaruk.com>
2017-09-15 22:32:31 +0300

    Kamera yapilandirmasi.

--- app/src/main/java/com/unalfaruk/sanatgalerisi/Main2Activity.java
+++ app/src/main/java/com/unalfaruk/sanatgalerisi/Main2Activity.java
@@ -3,11 +3,15 @@ package com.unalfaruk.sanatgalerisi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -20,7 +24,7 @@ public class Main2Activity extends AppCompatActivity {
 
     ImageView imgEser;
     String mCurrentPhotoPath;
-
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,10 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void fotografSec(View view){
-        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        dispatchTakePictureIntent();
+        /*Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePicture,1);
-        /*if(checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+        if(checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[] {Manifest.permission.CAMERA},2);
         }else{
             Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -49,8 +54,9 @@ public class Main2Activity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode==2){
             if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture,1);
+                /*Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture,1);*/
+                dispatchTakePictureIntent();
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -58,11 +64,10 @@ public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==1 && resultCode==RESULT_OK && data!=null){
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imgEser.setImageBitmap(imageBitmap);
+        if(requestCode==REQUEST_TAKE_PHOTO && resultCode==RESULT_OK && data!=null){
 
+            setPic();
+            Log.w("Yol: ",mCurrentPhotoPath);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -80,6 +85,54 @@ public class Main2Activity extends AppCompatActivity {
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
+        Log.w("Yol: ",mCurrentPhotoPath);
         return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imgEser.getWidth();
+        int targetH = imgEser.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        imgEser.setImageBitmap(bitmap);
     }
 }
